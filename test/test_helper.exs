4bcb28519962c6a646a8038cb57b0954bf9fbd28
Olle Linderos <olle@linderos.nu>
2021-06-26 01:45:23 +0200

    Add `ExpectFailure` test helper macro

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,18 @@
 ExUnit.start()
+
+defmodule ExpectFailure do
+  defmacro expect_failure(ast) do
+    quote do
+      try do
+        unquote(ast)
+        flunk("This should has crashed: " <> unquote(Macro.to_string(ast)))
+      rescue
+        e in ExUnit.AssertionError ->
+          raise e
+
+        _ ->
+          :ok
+      end
+    end
+  end
+end
