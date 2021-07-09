f986359394755f9d2acf4577bba42f97c2d964fb
Olle Linderos <olle@linderos.nu>
2021-07-09 12:47:37 +0200

    Add a check for unbalanced parens

--- lib/elixp.ex
+++ lib/elixp.ex
@@ -1,7 +1,4 @@
 defmodule Elixp do
-  @moduledoc """
-  """
-
   def parseAtom(str) do
     if String.length(str) == 0, do: raise "Invalid string"
     s = String.trim(str)
@@ -20,34 +17,38 @@ defmodule Elixp do
     parseList([], xs)
   end
 
+  # This is where the parsing ends
   def parseList(acc, [")" | xs]) do
     {Enum.reverse(acc), xs}
   end
 
+  # Transform '(abc def) into ["quote", "abc", "def"]
   def parseList(acc, ["'" | xs]) do
-    {y, _} = parseList([], xs)
-    parseList(acc, [["quote" | y]])
+    {y, ys} = parseList([], xs)
+    parseList([["quote" | List.flatten(y)], acc], ys)
   end 
 
+  # Create a new subtree
   def parseList(acc, ["(" | xs]) do
     {y, ys} = parseList([], xs)
     parseList([y | acc], ys)
   end 
-
-  # def parseList(acc, [x | xs]) when is_map(x) do
-  #   {y, ys} = parseList(x, xs)
-  #   parseList(acc, [y | ys])
-  # end 
-
+  
   def parseList(acc, [x | xs]) do
     parseList([parseAtom(x) | acc], xs)
   end 
-
+  
   def parseInput(str) do
     s = String.trim(str)
-    if String.length(str) == 0, do: raise "Invalid string"
-    if String.at(s, 0) != "(", do: raise "Missing opening parens"
-    if String.at(s, String.length(s) - 1) != ")", do: raise "Missing closing parens"
+
+    if String.length(str) == 0,
+      do: raise "Invalid string"
+    if String.at(s, 0) != "(",
+      do: raise "Missing opening parens"
+    if String.at(s, String.length(s) - 1) != ")",
+      do: raise "Missing closing parens"
+    if length(Regex.scan(~r/\(/, s)) != length(Regex.scan(~r/\)/, s)),
+      do: raise "Unbalanced parens"
 
     s
     |> String.replace("(", " ( ")
